54cd4acbfde8b822ce62031c4796c4a50c902111
Wesley95 <wesley.aquino95@outlook.com>
2020-10-25 00:07:42 -0300

    UpdateDeleteCreateUser

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,11 @@
-const express = require('express'); 
-const UsuarioController = require ('../controllers/usuarioControllers.js'); 
-const router = express.Router();  
+const express = require('express');
+const UsuarioController = require('../controllers/usuarioController.js');
+const router = express.Router();
 
-router.post('/usuarios', UsuarioController.Insert);  
+router.post('/usuarios', UsuarioController.Insert);
 router.get('/usuarios', UsuarioController.SelectAll);
 router.get('/usuarios/:id', UsuarioController.SelectDetail);
+router.put('/usuarios/:id', UsuarioController.Update);
+router.delete('/usuarios/:id', UsuarioController.Delete);
 
-module.exports = router; 
+module.exports = router;
