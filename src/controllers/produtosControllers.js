7d82d9232540b2d8e2382922c97806cce788be4e
Wesley95 <wesley.aquino95@outlook.com>
2020-11-04 21:55:21 -0300

    ProdutosUsuarios

---
+++ src/controllers/produtosControllers.js
@@ -0,0 +1,100 @@
+// Define a utilização do model usuario e a dependência http-status
+const Produto = require('../models/produtos');
+const status = require('http-status');
+
+// Cria o método Insert, obtendo os dados da request
+exports.Insert = (req, res, next) => {
+    const nome = req.body.nome;
+    const preco = req.body.preco;
+    const disponivel = req.body.disponivel;
+
+    // Popula cada um dos campos do model com os campos recebido na request
+    Produto.create({
+        nome: nome,
+        preco: preco,
+        disponivel: disponivel,
+    })
+        //then = registra o que queremos que aconteca quando a Promise for resolvida
+        .then(usuario => {
+            if (usuario) {
+                res.status(status.OK).send(usuario);
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        //catch = registra o que queremos que aconteca quando a Promise falhar
+        .catch(error => next(error));
+};
+
+exports.SelectAll = (req, res, next) => {
+    Produto.findAll()
+        .then(produto => {
+            if (produto) {
+                res.status(status.OK).send(produto);
+            }
+        })
+        .catch(error => next(error));
+}
+
+exports.SelectDetail = (req, res, next) => {
+    const id = req.params.id;
+
+    Produto.findByPk(id)
+        .then(produto => {
+            if (produto) {
+                res.status(status.OK).send(produto);
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
+
+exports.Update = (req, res, next) => {
+    const id = req.params.id;
+    const nome = req.body.nome;
+    const preco = req.body.preco;
+    const disponivel = req.body.disponivel;
+
+    Produto.findByPk(id)
+        .then(produto => {
+            if (produto) {
+                produto.update({
+                    nome: nome,
+                    preco: preco,
+                    disponivel: disponivel
+                },
+                    {
+                        where: { id: id }
+                    })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
+
+exports.Delete = (req, res, next) => {
+    const id = req.params.id;
+
+    Produto.findByPk(id)
+        .then(produto => {
+            if (produto) {
+                produto.destroy({
+                    where: { id: id }
+                })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            }
+            else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const UsuarioController = require('../controllers/usuarioControllers.js');
+const ProdutoController = require('../controllers/produtosControllers.js');
 const router = express.Router();
 
 router.post('/usuarios', UsuarioController.Insert);
@@ -7,5 +8,10 @@ router.get('/usuarios', UsuarioController.SelectAll);
 router.get('/usuarios/:id', UsuarioController.SelectDetail);
 router.put('/usuarios/:id', UsuarioController.Update);
 router.delete('/usuarios/:id', UsuarioController.Delete);
+
+router.post('/produtos', ProdutoController.Insert);
+router.get('/produtos/:id', ProdutoController.SelectDetail);
+router.get('/produtos', ProdutoController.SelectAll);
+router.delete('/produtos/:id', ProdutoController.Delete);
 
 module.exports = router;
