c8c7c717ea667dd7e5e63166b14d0caee2d98a8e
Wesley95 <wesley.aquino95@outlook.com>
2020-11-10 00:09:33 -0300

    BancoAWS

--- src/config/config.js
+++ src/config/config.js
@@ -1,20 +1,27 @@
-//Define a string de conexão ao banco de dados.
-module.exports = { 
+require('dotenv/config');
 
-    development: { 
-        database: { 
-            host: 'localhost', 
-            port: 3306, 
-            name: 'db_usuario', 
-            dialect: 'mysql', 
-            user: 'root', 
-            password: null
-        } 
-    }, 
-    production:{ 
-        database: { 
-            host: process.env.DB_HOST, 
-            host: process.env.DB_PORT 
-        } 
-    } 
-} 
+
+console.log(process.env.DB_DIALECT);
+// Define a string de conexão com o banco de dados
+module.exports = {
+    development: {
+        database: {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            name: process.env.DB_NAME,
+            dialect: process.env.DB_DIALECT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD
+        }
+    },
+    production: {
+        database: {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            name: process.env.DB_NAME,
+            dialect: process.env.DB_DIALECT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD
+        }
+    }
+}
